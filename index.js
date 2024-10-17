61a4c8eb3dfdc1f5fd40ac5154a85d22cb6dcb93
js souza <00001106142871sp@al.educacao.sp.gov.br>
2024-10-17 13:49:35 -0300

    mudanças da aparencia

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const avanca = document.querySelectorAll(".btn-proximo");
 avanca.forEach(button => {
-    button.addEventListener("click", function())
-    
+    button.addEventListener("click", function(){
+        const atual = document.querySelector(".ativo");
+        const proximoPasso = "passo-" + this.getAttribute("data-proximo");
+        atual.classList.remove("ativo");
+        document.getElementaryId(proximopasso).classList.add("ativo");
 
+    })
+    
 })
